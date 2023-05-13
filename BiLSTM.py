33b7c35b0d4be3583190ea08ca29a76ca40982fb
Aldi Halili <96257594+itsmeeeeeee@users.noreply.github.com>
2023-05-13 12:13:41 +0200

    Add files via upload

---
+++ BiLSTM.py
@@ -0,0 +1,155 @@
+import data_preprocessing
+from data_preprocessing import *
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as data
+
+
+Xtrain = torch.tensor(X_train,dtype=torch.long)
+Xtest = torch.tensor(X_test,dtype=torch.long)
+ytrain = torch.tensor(y_train,dtype=torch.long)
+ytest = torch.tensor(y_test, dtype=torch.long)
+Xdev = torch.tensor(X_dev,dtype=torch.long)
+ydev = torch.tensor(y_dev,dtype=torch.long)
+
+
+print(len(Xtrain))
+print(len(ytest.view(-1)))
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device
+
+
+loader_train = data.DataLoader(data.TensorDataset(Xtrain, ytrain), shuffle=True, batch_size=4)
+loader_dev = data.DataLoader(data.TensorDataset(Xdev, ydev), shuffle=True, batch_size=4)
+loader_test = data.DataLoader(data.TensorDataset(Xtest, ytest), shuffle=True, batch_size=4)
+
+
+class BiLSTM(nn.Module):
+    def __init__(self, vocab_size, embedding_dim, hidden_dim, output_dim):
+        super().__init__()
+        
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, num_layers=2, bidirectional=True, dropout=0.5)
+        self.fc = nn.Linear(hidden_dim * 2, output_dim)
+        
+        
+    def forward(self, text):
+        embedded = self.embedding(text)
+        output,_ = self.lstm(embedded)
+       
+       
+        tag_scores = self.fc(output)
+        return tag_scores
+
+
+
+EMBEDDING_DIM = 100
+HIDDEN_DIM = 128
+OUTPUT_DIM = TAG_COUNT
+#NUM_LAYERS=2
+
+
+class EarlyStopping:
+    def __init__(self, tolerance=0.001, min_delta= 0.001):
+
+        self.tolerance = tolerance
+        self.min_delta = min_delta
+        self.counter = 0
+        self.early_stop = False
+
+    def __call__(self, train_loss, validation_loss):
+        if (validation_loss - train_loss) > self.min_delta:
+            self.counter +=1
+            if self.counter >= self.tolerance:  
+                self.early_stop = True
+
+
+
+model_lstm=BiLSTM(len_uniq_words,EMBEDDING_DIM,HIDDEN_DIM,OUTPUT_DIM).to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer_lstm = optim.Adam(model_lstm.parameters())
+
+
+num_epochs=10
+for epoch in range(num_epochs):
+    model_lstm.train()
+    train_loss=0.0
+    #print(epoch)
+    for x_batch, y_batch in loader_train:
+    #for x in Xtrain:
+        
+        optimizer_lstm.zero_grad()
+        tag_scores = model_lstm(x_batch)
+        predictions=tag_scores.view(-1,tag_scores.shape[-1])
+        
+        tags=y_batch.view(-1)
+        #print("true tag",tags)
+        loss = criterion(predictions, tags)
+        loss.backward()
+        optimizer_lstm.step()
+        train_loss += loss.item()
+    train_loss/=len(loader_train)
+   
+
+    #Evaluation phase
+    model_lstm.eval()
+    dev_loss=0.0
+    with torch.no_grad():
+      for x_dev_batch,y_dev_batch in loader_dev:
+        tag_scores = model_lstm(x_dev_batch)
+        predictions=tag_scores.view(-1,tag_scores.shape[-1])
+        
+        tags=y_dev_batch.view(-1)
+        loss = criterion(predictions, tags)
+
+        dev_loss+=loss.item()
+    dev_loss /= len(loader_dev)
+
+        
+   
+    early_stopping = EarlyStopping(tolerance=3, min_delta= 0.1)
+
+
+    early_stopping(train_loss, dev_loss)
+  
+    print(f"Epoch {epoch + 1}, Train Loss: {train_loss}, Dev Loss: {dev_loss}")
+    if early_stopping.early_stop:
+        print("We are at epoch:", epoch)
+        break
+    
+
+
+def decode_tag(predictions,idtotag):
+  decoded_tags=[idtotag[int(p)] for p in predictions]
+  return decoded_tags
+
+
+# Evaluate the model on test data
+model_lstm.eval()
+all_preds=[]
+all_true_tags=[]
+
+with torch.no_grad():
+  for x_batch_test, y_batch_test in loader_test:
+    tag_scores = model_lstm(x_batch_test)
+    predictions=tag_scores.view(-1,tag_scores.shape[-1])
+    max_pred=predictions.argmax(dim=-1)
+    true_tags=y_batch_test.view(-1)
+    decoded_pred=decode_tag(max_pred,id2tag)
+    decoded_true_tag=decode_tag(true_tags,id2tag)
+    all_preds.extend(decoded_pred)
+    all_true_tags.extend(decoded_true_tag)
+#print(all_preds)
+#print(all_true_tags)
+accuracy= accuracy_score(all_true_tags,all_preds)
+precision = precision_score(all_true_tags, all_preds, average="weighted",zero_division=0)
+#recall=recall_score(all_true_tags, all_preds)
+#f1=f1_score(all_true_tags, all_preds)
+
+print(f"Accuracy TestData: {accuracy:.2f}")
+print(f"Precision TestData: {precision}")
--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,25 +1,12 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-
-
-# In[2]:
-
+#from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+import io
 
 # read the annotated dataset
 data_df = pd.read_csv("ner_datasetreference.csv", encoding="iso-8859-1", header=0)
 data_df.head()
-
-
-# In[3]:
-
 
 #replace zero values with last valid observation
 
@@ -27,56 +14,31 @@ data_df = data_df.fillna(method="ffill")
 
 data_df.head()
 
-
-# In[4]:
-
-
 # count number of sentences and number of words
-
 #print("Total number of sentences in the dataset:", data_df["Sentence #"].nunique())
 #print("Total words in the dataset:", (data_df.shape[0]))
 
-
-# In[5]:
-
-
 # count labels
-
 data_df[data_df["Tag"]!="O"]["Tag"].value_counts()
-
-
-# In[6]:
-
 
 # count words for each sentences
 word_counts = data_df.groupby("Sentence #")["Word"].agg(["count"])
 #print(word_counts)
 
-
-# In[7]:
-
-
 # define the longest sentence in the corpus
 MAX_SENTENCE = word_counts.max()[0]
 #print(MAX_SENTENCE)
 
-
-# In[8]:
-
-
 # define number of unique words and unique tags
 uniq_words = list(set(data_df["Word"].values))
 uniq_tags = list(set(data_df["Tag"].values))
-len_uniq_words = len(uniq_words)                   
+                  
 
-len_uniq_tag=len(uniq_tags)
+#len_uniq_tag=len(uniq_tags)
 #len_uniq_tag
 
 
 # Implement the necessary feature engineering.
-
-# In[9]:
-
 
 """ 
     build a dictionary (word2id) that assigns a unique integer value to every word from the corpus and 
@@ -91,16 +53,12 @@ word2id["--UNKNOWN_WORD--"]=0
 word2id["--PADDING--"]=1
 
 id2word = {idx: word for word, idx in word2id.items()}
+len_uniq_words = len(id2word)
 
+print(len_uniq_words)
 
-# In[10]:
-
-
-print(word2id["Crumpton"])
-print(id2word[24640])
-
-
-# In[11]:
+#print(word2id["Crumpton"])
+#print(id2word[24640])
 
 
 #  build a similar dictionary for the various tags
@@ -108,14 +66,7 @@ tag2id = {tag: idx + 1 for idx, tag in enumerate(uniq_tags)}
 tag2id["--PADDING--"] = 0
 id2tag = {idx: word for word, idx in tag2id.items()}
 
-
-# In[12]:
-
-
-print(id2tag)
-
-
-# In[13]:
+#print(id2tag)
 
 
 def create_tuples(data):
@@ -130,10 +81,7 @@ def create_tuples(data):
 # apply this function to the entire dataset
 sentences = data_df.groupby("Sentence #").apply(create_tuples).tolist()
 
-print(sentences[0])
-
-
-# In[14]:
+#print(sentences[0])
 
 
 """  
@@ -144,44 +92,26 @@ print(sentences[0])
 
 X = [[word[0] for word in sentence] for sentence in sentences]
 y = [[word[2] for word in sentence] for sentence in sentences]
-
 #print("X[0]:", X[0])
 #print("y[0]:", y[0])
-
-
-# In[15]:
 
 
 # replace each word with its corresponding index from the dictionary
 
 X = [[word2id[word] for word in sentence] for sentence in X]
 y = [[tag2id[tag] for tag in sentence] for sentence in y]
-
 #print("X[0]:", X[0])
 #print("y[0]:", y[0])
-
-
-# In[16]:
-
 
 # for the LSTM model to process input of consistent length, eauch sentence should be padded to match the longest sentence
 
 X = [sentence + [word2id["--PADDING--"]] * (MAX_SENTENCE - len(sentence)) for sentence in X]
 y = [sentence + [tag2id["--PADDING--"]] * (MAX_SENTENCE - len(sentence)) for sentence in y]
-
 #print("X[0]:", X[0])
 #print("y[0]:", y[0])
 
-
-# In[17]:
-
-
 TAG_COUNT = len(tag2id)
-
-
-# In[19]:
-
-
+print(TAG_COUNT)
 
 # split data in test, development udn train 
 X_main, X_test, y_main, y_test = train_test_split(X, y, test_size=0.1, random_state=1234)
@@ -191,10 +121,3 @@ X_train,X_dev,y_train,y_dev=train_test_split(X_main, y_main, test_size=0.1, rand
 #print("Number of sentences in the test dataset : {}".format(len(X_test)))
 
 #print(X_train)
-
-
-# In[ ]:
-
-
-
-
